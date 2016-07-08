525e00647c281fbcac3e85c298d118581f9dde27
marco <marco.craveiro@gmail.com>
2016-07-08 15:46:15 +0100

    scader: link up the various workflows to produce scad files

--- projects/scader/src/main.cpp
+++ projects/scader/src/main.cpp
@@ -19,18 +19,33 @@
  *
  */
 #include <iostream>
+#include "neurite/utility/log/logger.hpp"
+#include "neurite/utility/filesystem/file.hpp"
+#include "neurite/utility/log/life_cycle_manager.hpp"
 #include "neurite/swc/types/hydrator.hpp"
 #include "neurite/swc/types/tree_factory.hpp"
 #include "neurite/geometry/types/scad_evaluator.hpp"
+#include "neurite/geometry.swc/types/transformer.hpp"
 #include "neurite/scader/program_options_parser.hpp"
 #include "neurite/scader/parser_validation_error.hpp"
 
 namespace {
 
+using namespace neurite::utility::log;
+auto lg(logger_factory("scader"));
+
+const std::string log_file_prefix("log/neurite.scader.");
 const std::string more_information(
     "Try `neurite.scader --help' for more information.");
 const std::string usage_error_msg("Usage error: ");
 
+}
+
+void initialise_logging(const std::string& name, const bool verbose) {
+    const auto sev(verbose ? severity_level::debug : severity_level::info);
+    const auto log_file_name = log_file_prefix + name + ".log";
+    life_cycle_manager lcm;
+    lcm.initialise(log_file_name, sev);
 }
 
 int main(const int argc, const char* argv[]) {
@@ -43,6 +58,25 @@ int main(const int argc, const char* argv[]) {
          */
         if (!o)
             return 0;
+
+        const auto source(o->first);
+        initialise_logging(source.stem().string(), true/*verbose*/);
+
+        neurite::swc::hydrator h;
+        const auto m(h.hydrate(source));
+
+        neurite::swc::tree_factory tf;
+        const auto swc_tree(tf.build(m));
+
+        neurite::geometry::swc::transformer t;
+        const auto geometry_tree(t.transform(swc_tree));
+
+        neurite::geometry::scad_evaluator e;
+        const auto scad(e.evaluate(geometry_tree));
+
+        const auto destination(o->second);
+        neurite::utility::filesystem::write_file_content(destination, scad);
+
     } catch (const neurite::scader::parser_validation_error& e) {
         std::cerr << usage_error_msg << e.what() << std::endl
                   << more_information << std::endl;
